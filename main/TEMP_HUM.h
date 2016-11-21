bc7c8448aee7d0e4ad49e2dc1b46a1a025e395ba
Surya Penmetsa <surypenmetsa@Hotmail.com>
2016-11-21 17:57:40 +0530

    Initial Commit

---
+++ main/TEMP_HUM.h
@@ -0,0 +1,45 @@
+#include "dht.h"
+dht DHT;
+#define DHT11_PIN 5
+
+float TEMP=0.0;
+float HUMI=0.0;
+
+void get_temp_hum ()
+{
+  // READ DATA
+  Serial.print("DHT11, \t");
+  int chk = DHT.read11(DHT11_PIN);
+  Serial.println("Type,\tstatus,\tHumidity (%),\tTemperature (C)");
+  switch (chk)
+  {
+    case DHTLIB_OK:  
+                Serial.print("OK,\t"); 
+                break;
+    case DHTLIB_ERROR_CHECKSUM: 
+                Serial.print("Checksum error,\t"); 
+                break;
+    case DHTLIB_ERROR_TIMEOUT: 
+                Serial.print("Time out error,\t"); 
+                break;
+    case DHTLIB_ERROR_CONNECT:
+        Serial.print("Connect error,\t");
+        break;
+    case DHTLIB_ERROR_ACK_L:
+        Serial.print("Ack Low error,\t");
+        break;
+    case DHTLIB_ERROR_ACK_H:
+        Serial.print("Ack High error,\t");
+        break;
+    default: 
+                Serial.print("Unknown error,\t"); 
+                break;
+  }
+  // DISPLAY DATA
+  Serial.print(DHT.humidity, 1);
+  HUMI=DHT.humidity;
+  Serial.print(",\t");
+  Serial.println(DHT.temperature, 1);
+  TEMP=DHT.temperature;
+}
+
